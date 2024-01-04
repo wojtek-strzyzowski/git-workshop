62bde071958fb7b48f7a0d20f7e43da4b1a012cd
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-01-04 15:36:16 +0100

    sth

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     gsap.from(".animated-heading", { opacity: 0, y: 20, duration: 1, delay: 0.5, ease: "power4.out" });
 });
 
+function summation(a, b){
+    return a+b;
+}
