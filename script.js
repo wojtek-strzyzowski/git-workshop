cd954c44f409cefee00a34a40e8bc4c2ad93b779
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-01-04 15:31:35 +0100

    zwei branchen namen ari und wojtek

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.addEventListener("DOMContentLoaded", function() {
     // Animation für Überschriften mit der Klasse 'animated-heading'
     gsap.from(".animated-heading", { opacity: 0, y: 20, duration: 1, delay: 0.5, ease: "power4.out" });
 });
+
