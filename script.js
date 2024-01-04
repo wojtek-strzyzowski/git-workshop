e0932e20f53a5359c32f4729b457438cf4d25a79
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-01-04 15:45:34 +0100

    herr wojtek hat seine seite auch fertig

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Animation für Überschriften mit der Klasse 'animated-heading'
     gsap.from(".animated-heading", { opacity: 0, y: 20, duration: 1, delay: 0.5, ease: "power4.out" });
 });
+
+function multiplication(a, n){
+    return a*n;
+}
